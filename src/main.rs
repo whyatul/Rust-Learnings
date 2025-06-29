cdcbcfb88d6787cfa53b5ae307521132b9c2086b
Atul Maurya <atulmaurya18.04@gmail.com>
2025-06-30 02:06:12 +0530

    Initialize Rust project with Cargo configuration and main application file

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let x: i32 = 4543;
+    println!("The value of x is: {} This tells that x is a signed integer of 32 bits ", x);
+    let y: f32 = 454.3000322;
+    println!("The value of y is: {} This tells that x is a float of 32 bits ", y);
+}
