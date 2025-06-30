893203d5826cd81afe559923fd0a2d20870fde94
Atul Maurya <atulmaurya18.04@gmail.com>
2025-06-30 08:50:00 +0530

    Update output message to clarify the returned value from get_first_func

--- src/function.rs
+++ src/function.rs
@@ -1,7 +1,7 @@
 fn main(){ //main Function
     let sentence: String = String::from("Hello! my name is Atul");
     let first_func: String = get_first_func(sentence);
-    println!("The Function Returns the value: {}", first_func);
+    println!("The Function Returns the value the first word of the sentence: {}", first_func);
 }
 fn get_first_func(sentence: String) -> String {
     let mut ans: String = String::from("");
