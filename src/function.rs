c8d57d408b4921253abaffeb6954035f183d4eaf
Atul Maurya <atulmaurya18.04@gmail.com>
2025-06-30 08:45:04 +0530

    Add main function and get_first_func to process a sentence

---
+++ src/function.rs
@@ -0,0 +1,15 @@
+fn main(){ //main Function
+    let sentence: String = String::from("Hello! my name is Atul");
+    let first_func: String = get_first_func(sentence);
+    println!("The Function Returns the value: {}", first_func);
+}
+fn get_first_func(sentence: String) -> String {
+    let mut ans: String = String::from("");
+    for char in sentence.chars(){
+        ans.push_str(char.to_string().as_str());
+        if char == ' ' {            
+            break;
+        }
+    }
+    return ans;
+}
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,21 @@ fn main() {
     println!("The value of x is: {} This tells that x is a signed integer of 32 bits ", x);
     let y: f32 = 454.3000322;
     println!("The value of y is: {} This tells that x is a float of 32 bits ", y);
+
+    
+    //Booleans
+    let is_male:bool = true;
+    let is_18:bool = true;
+
+    if(is_male){
+        println!("The value of male");
+    }
+    else{
+        println!("The vlaue is not male");
+    }
+
+    if is_18 && is_male{
+        println!("The Presoon is legal to Vote");
+    }
+
 }
